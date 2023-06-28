d4003d0f2a355e34960bbe3e5d32d01c9e779101
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-06-28 10:38:09 +0200

    mainWindow

--- main.js
+++ main.js
@@ -1,44 +1,30 @@
-const { app, BrowserWindow, ipcMain } = require('electron')
+const {app, BrowserWindow} = require('electron')
 const path = require('path')
 
-let mainWindow
-
-function createWindow () {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
+function createWindows () {
+    // Create the score window.
+    let mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 960,
         webPreferences: {
             nodeIntegration: true,
             contextIsolation: false,
-            enableRemoteModule: true
-        },
-        show: false // Ne pas afficher la fenêtre lors du démarrage
+        }
     })
 
-    mainWindow.loadFile(path.join(__dirname, 'index.html'))
-
-    // Ouvrez les outils de développement de Chrome.
-    mainWindow.webContents.openDevTools()
-
-    mainWindow.on('closed', function () {
-        mainWindow = null
-    })
+    mainWindow.loadFile('index.html')
 }
 
-app.on('ready', createWindow)
+app.whenReady().then(createWindows)
 
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') app.quit()
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
-app.on('activate', function () {
-    if (mainWindow === null) createWindow()
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindows()
+    }
 })
-
-ipcMain.on('control-message', (event, arg) => {
-    // Répondre à un message de contrôle de l'application React
-    // Vous pouvez utiliser `mainWindow.webContents.send(...)` pour envoyer
-    // des données à l'afficheur de l'application Electron.
-    console.log(arg);
-})
-
