83f077db2e47889dc6d594f9382346bd3115b058
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-10-10 12:11:02 +0200

    debugging

--- src/Components/MediaMode.js
+++ src/Components/MediaMode.js
@@ -5,39 +5,42 @@ const MediaMode = ({ mediaState }) => {
     const [currentMediaIndex, setCurrentMediaIndex] = useState(0);
 
     useEffect(() => {
-        if (!mediaState || mediaState.length === 0) {
+        if (!Array.isArray(mediaState) || mediaState.length === 0) {
             console.log("No media available");
             return;
         }
 
+        const currentMedia = mediaState[currentMediaIndex];
+        const duration = (currentMedia && typeof currentMedia.duration === 'number') ? currentMedia.duration * 1000 : 5000; // Default to 5 seconds if not provided
+
         const timer = setTimeout(() => {
             setCurrentMediaIndex((currentMediaIndex + 1) % mediaState.length);
-        }, mediaState[currentMediaIndex].duration * 1000);
+        }, duration);
 
         return () => clearTimeout(timer);
     }, [currentMediaIndex, mediaState]);
 
-    if (!mediaState || mediaState.length === 0) {
+    if (!Array.isArray(mediaState) || mediaState.length === 0 || !mediaState[currentMediaIndex]) {
         return <div>No media available</div>;
     }
 
     const currentMedia = mediaState[currentMediaIndex];
+    const isVideo = currentMedia.type === "video";
+    const mediaPath = currentMedia.path || ''; // Default to empty string if path is not provided
 
     return (
         <div className="container">
-            {currentMedia.type === "video" ? (
+            {isVideo ? (
                 <video
-                    src={currentMedia.path}
+                    src={mediaPath}
                     autoPlay
                     onEnded={() => setCurrentMediaIndex((currentMediaIndex + 1) % mediaState.length)}
                 />
             ) : (
-                <img src={"../../Frontend/public" + mediaState[currentMediaIndex].path} alt="Media content" />
-
+                <img src={"../../Frontend/public" + mediaPath} alt="Media content" />
             )}
         </div>
     );
 };
 
 export default MediaMode;
-
