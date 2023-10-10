656e1588e325fc0939a6d8e090017ac658aeaa16
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-10-10 14:37:46 +0200

    debugging

--- src/Components/Sports/StandardDisplay.js
+++ src/Components/Sports/StandardDisplay.js
@@ -5,6 +5,7 @@ import "./StandardDisplay.css";
 const StandardDisplay = ({gameState}) => {
     const periodOrSet = gameState.Period || gameState.Set;
     const timer = gameState.Timer ? gameState.Timer.Value : "00:00";
+    const possession = gameState.Possession ? gameState.Possession : "Home";
     const homeTeamName = gameState.Home ? gameState.Home.TeamName : "Home";
     const guestTeamName = gameState.Guest ? gameState.Guest.TeamName : "Guest";
     const homeTeamScore = gameState.Home ? gameState.Home.Points : "0";
@@ -16,25 +17,30 @@ const StandardDisplay = ({gameState}) => {
     return (
         <div class="scoreboard">
             <div class="team-display home">
-                <span class="team-score-display">999</span>
-                <span class="team-name-display">Domicile</span>
+                <span class="team-score-display">{homeTeamName}</span>
+                <span class="team-name-display">{homeTeamScore}</span>
 
                 <div className="timeout-left">
-                    <div className="circleIcon"></div>
-                    <div className="circleIcon"></div>
-                    <div className="circleIcon"></div>
+                  {
+                    Array.from(Array(homeTeamTimeouts), (e, i) => {
+                        return <div className="circleIcon"></div>
+                    })
+                }
                 </div>
                 <div class="timer-timeout">00:00</div>
             </div>
 
             <div class="middle-section">
                 <div class="period">
-                    <div className="arrow-icon-left"></div>
+                  {possession === "Home" ? <div className="possession-icon-left"></div> : null }
+                    {/*<div className="arrow-icon-left"></div>*/}
 
-                    <span class="period-number">1</span>
-                    <div className="arrow-icon-right"></div>
+                    <span class="period-number">{periodOrSet}</span>
+
+                  {possession === "Guest" ? <div className="possession-icon-right"></div> : null}
+                    {/*<div className="arrow-icon-right"></div>*/}
                 </div>
-                <div class="timer">00:00</div>
+                <div class="timer">{timer}</div>
                 <img
                     className="logo"
                     src="images/_Stramatel_Logo_FR_2.png"
@@ -43,13 +49,15 @@ const StandardDisplay = ({gameState}) => {
             </div>
 
             <div class="team-display guest">
-                <span class="team-score-display">999</span>
-                <span class="team-name-display">Invité</span>
+                <span class="team-score-display">{guestTeamScore}</span>
+                <span class="team-name-display">{guestTeamName}</span>
 
                 <div className="timeout-right">
-                    <div className="circleIcon"></div>
-                    <div className="circleIcon"></div>
-                    <div className="circleIcon"></div>
+                    {
+                  Array.from(Array(guestTeamTimeouts), (e, i) => {
+                  return <div className="circleIcon"></div>
+                })
+                  }
                 </div>
                 <div class="timer-timeout">00:00</div>
             </div>
