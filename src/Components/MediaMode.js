20565d8b73b48f0a964bf251508ee970f9e1e6d9
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-10-10 16:13:37 +0200

    debugging

--- src/Components/MediaMode.js
+++ src/Components/MediaMode.js
@@ -37,6 +37,7 @@ const MediaMode = ({mediaState}) => {
                     style={{width: "100%", height: "100%"}}
                     src={mediaPath}
                     autoPlay
+                    preload={"auto"}
                     onEnded={() => setCurrentMediaIndex((currentMediaIndex + 1) % mediaState.length)}
                 />
             ) : (
